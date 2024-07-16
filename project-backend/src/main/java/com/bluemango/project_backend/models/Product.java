77e06a5ade9e59639d14eda04aca92ef2a921c06
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-07-16 17:27:06 -0300

    hashcode, equals and ToString methods of override

--- project-backend/src/main/java/com/bluemango/project_backend/models/Product.java
+++ project-backend/src/main/java/com/bluemango/project_backend/models/Product.java
@@ -1,5 +1,7 @@
 package com.bluemango.project_backend.models;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +11,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TBL_PRODUCT")
-public class Product {
+public class Product implements Serializable {
     
     //Atributes
     @Id
@@ -96,6 +98,37 @@ public class Product {
 
     public void setNovo(boolean novo) {
         this.novo = novo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", desc=" + desc + ", price=" + price + ", promotion="
+                + promotion + ", novo=" + novo + "]";
     }
 
     
